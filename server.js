e121edc7ebb490d42212e021168b58303db721ab
cstudio7 <conquerorsword36@yahoo.com>
2021-09-13 03:44:23 +0100

    node killer addedd

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 
 app.get('/', function (req, res) {
-    res.send('Its a beautiful Thing Learning CI/CD with Victor');
+    res.send('Its a beautiful Thing Learning CI/CD with Victor and Chiamaka');
 });
 
 const server = app.listen(port, () => {
